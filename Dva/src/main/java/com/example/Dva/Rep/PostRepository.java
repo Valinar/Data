34351f2ea0f9a863907e1e0c60930a7adfae40f0
Valinar <107581088+Valinar@users.noreply.github.com>
2022-09-06 20:24:59 +0300

    Add files via upload

---
+++ Dva/src/main/java/com/example/Dva/Rep/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.Dva.Rep;
+
+import com.example.Dva.models.Data;
+import com.example.Dva.models.Data2;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface PostRepository extends CrudRepository<Data, Long> {
+    List <Data>  findByName(String name);
+}
